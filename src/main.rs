92cc7eca8a42eea3d6cf10dee15ead0b14c5f22c
Griffin T <a12345678910ster@gmail.com>
2020-08-18 00:00:36 +0800

    Decode handshake packet

--- src/main.rs
+++ src/main.rs
@@ -55,13 +55,60 @@ impl <R: Read> MCPacketReader<R> {
         let (id, id_length) = self.read_varint();
         let data = self.next_bytes((length as usize) - id_length);
 
-        PacketBuffer {id, data}
+        PacketBuffer::new(id, data)
     }
 }
 
 struct PacketBuffer {
     id: i32,
-    data: Vec<u8>
+    data: Vec<u8>,
+    index: usize,
+}
+
+impl PacketBuffer {
+    fn new(id: i32, data: Vec<u8>) -> PacketBuffer {
+        PacketBuffer {
+            id,
+            data,
+            index: 0,
+        }
+    }
+
+    fn read_varint(&mut self) -> i32 {
+        let mut acc = 0;
+        let mut i = 0;
+
+        loop {
+            let byte = self.data[self.index + i] as i32;
+            acc |= ( byte & 0x7F ) << i * 7;
+            
+            i += 1;
+            if i > 5 {
+                panic!( "varint too big" );
+            }
+
+            if ( byte & 0b10000000 ) == 0 {
+                break;
+            }
+        }
+
+        self.index += i;
+        acc
+    }
+
+    fn read_string(&mut self) -> String {
+        let length = self.read_varint() as usize;
+        let data = self.data.get(self.index .. self.index + length).unwrap();
+        self.index += length;
+        String::from_utf8(data.to_vec()).unwrap()
+    }
+
+    fn read_unsigned_short(&mut self) -> u16 {
+        let mut num = ((self.data[self.index] as u16) << 8) +
+            (self.data[self.index + 1] as u16);
+        self.index += 2;
+        num
+    } 
 }
 
 fn main() {
@@ -73,7 +120,18 @@ fn main() {
 
         let mut reader = MCPacketReader::new(stream);
         
-        let handshake = reader.next_packet();
-        println!("handshake (id: {}, data: {:?})", handshake.id, handshake.data);
+        let mut handshake = reader.next_packet();
+
+        let protocol_version = handshake.read_varint();
+        let server_address = handshake.read_string();
+        let server_port = handshake.read_unsigned_short();
+        let next_state = handshake.read_varint();
+
+        println!("handshake (id: {}, data: {:?}) {{", handshake.id, handshake.data);
+        println!("\tprotocol version = {}", protocol_version);
+        println!("\tserver address = {}", server_address);
+        println!("\tserver port = {}", server_port);
+        println!("\tnext state = {}", next_state);
+        println!("}}");
     }
 }
