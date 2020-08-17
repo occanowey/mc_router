a9100e22a80554e80f4b9e5b016049e38358b727
Griffin T <a12345678910ster@gmail.com>
2020-08-18 02:22:36 +0800

    Remove unnessacery structs

--- src/main.rs
+++ src/main.rs
@@ -1,114 +1,29 @@
 use std::{
-    io::{Read, Write},
+    io::{BufReader, Read, Write},
     net::{TcpListener, TcpStream},
     sync::{Arc, RwLock},
     thread,
 };
 
-struct MCPacketReader<R: Read> {
-    inner: R,
-    buffer: Vec<u8>,
-}
+fn read_varint(offset: usize, src: &[u8]) -> (i32, usize) {
+    let mut acc = 0;
+    let mut i = 0;
 
-impl<R: Read> MCPacketReader<R> {
-    fn new(inner: R) -> MCPacketReader<R> {
-        MCPacketReader {
-            inner: inner,
-            buffer: Vec::new(),
+    loop {
+        let byte = src[offset + i] as i32;
+        acc |= (byte & 0x7F) << (i * 7);
+
+        i += 1;
+        if i > 5 {
+            panic!("varint too big");
+        }
+
+        if (byte & 0b10000000) == 0 {
+            break;
         }
     }
 
-    fn next_bytes(&mut self, length: usize) -> Vec<u8> {
-        if self.buffer.len() < length {
-            let mut chunk = [0; 16];
-            let length = self.inner.read(&mut chunk).unwrap();
-            self.buffer.write(&chunk[0..length]).unwrap();
-        }
-
-        let remaining = self.buffer.split_off(length);
-        std::mem::replace(&mut self.buffer, remaining)
-    }
-
-    fn next_byte(&mut self) -> u8 {
-        self.next_bytes(1)[0]
-    }
-
-    fn read_varint(&mut self) -> (i32, usize) {
-        let mut acc = 0;
-        let mut i = 0;
-
-        loop {
-            let byte = self.next_byte() as i32;
-            acc |= (byte & 0x7F) << i * 7;
-
-            i += 1;
-            if i > 5 {
-                panic!("varint too big");
-            }
-
-            if (byte & 0b10000000) == 0 {
-                break;
-            }
-        }
-
-        (acc, i)
-    }
-
-    fn next_packet(&mut self) -> PacketBuffer {
-        let (length, _) = self.read_varint();
-
-        let (id, id_length) = self.read_varint();
-        let data = self.next_bytes((length as usize) - id_length);
-
-        PacketBuffer::new(id, data)
-    }
-}
-
-struct PacketBuffer {
-    id: i32,
-    data: Vec<u8>,
-    index: usize,
-}
-
-impl PacketBuffer {
-    fn new(id: i32, data: Vec<u8>) -> PacketBuffer {
-        PacketBuffer { id, data, index: 0 }
-    }
-
-    fn read_varint(&mut self) -> i32 {
-        let mut acc = 0;
-        let mut i = 0;
-
-        loop {
-            let byte = self.data[self.index + i] as i32;
-            acc |= (byte & 0x7F) << i * 7;
-
-            i += 1;
-            if i > 5 {
-                panic!("varint too big");
-            }
-
-            if (byte & 0b10000000) == 0 {
-                break;
-            }
-        }
-
-        self.index += i;
-        acc
-    }
-
-    fn read_string(&mut self) -> String {
-        let length = self.read_varint() as usize;
-        let data = self.data.get(self.index..self.index + length).unwrap();
-        self.index += length;
-        String::from_utf8(data.to_vec()).unwrap()
-    }
-
-    fn read_unsigned_short(&mut self) -> u16 {
-        let mut num = ((self.data[self.index] as u16) << 8) + (self.data[self.index + 1] as u16);
-        self.index += 2;
-        num
-    }
+    (acc, i)
 }
 
 fn main() {
@@ -123,16 +38,49 @@ fn main() {
 }
 
 fn handle_client(client: TcpStream) {
-    let mut reader = MCPacketReader::new(client);
+    let mut client = BufReader::new(client);
 
-    let mut packet = reader.next_packet();
+    let mut index = 0;
+    // 7 because it's the minimum length of a handshake packet but more than a varint :)
+    let mut buffer = vec![0u8; 7];
+    client.read_exact(&mut buffer).unwrap();
 
-    let protocol_version = packet.read_varint();
-    let server_address = packet.read_string();
-    let server_port = packet.read_unsigned_short();
-    let next_state = packet.read_varint();
+    // todo: handle legacy ping
 
-    println!("handshake (id: {}, data: {:?}) {{", packet.id, packet.data);
+    let (length, length_length) = read_varint(index, &buffer);
+    index += length_length;
+
+    // length of the packet - what's already in the buffer
+    let chunk_length = (length as usize) - (buffer.len() - length_length);
+    let mut chunk = vec![0; chunk_length];
+    client.read_exact(&mut chunk).unwrap();
+    buffer.append(&mut chunk);
+
+    let (id, id_length) = read_varint(index, &buffer);
+    index += id_length;
+
+    if id != 0 {
+        panic!("Invalid packet id recieved from client.");
+    }
+
+    let (protocol_version, protocol_version_length) = read_varint(index, &buffer);
+    index += protocol_version_length;
+
+    // messy
+    let server_address = {
+        let (length, length_length) = read_varint(index, &buffer);
+        index += length_length;
+        let data = &buffer[index..index + length as usize];
+        index += length as usize;
+        String::from_utf8(data.to_vec()).unwrap()
+    };
+
+    let server_port = ((buffer[index] as u16) << 8) | buffer[index + 1] as u16;
+    index += 2;
+
+    let (next_state, _) = read_varint(index, &buffer);
+
+    println!("handshake (id: {}) {{", id);
     println!("\tprotocol version = {}", protocol_version);
     println!("\tserver address = {}", server_address);
     println!("\tserver port = {}", server_port);
@@ -140,11 +88,10 @@ fn handle_client(client: TcpStream) {
     println!("}}");
 
     let mut server = TcpStream::connect("127.0.0.1:25565").unwrap();
-    server.write(&[16, packet.id as u8]);
-    server.write(&packet.data);
-    server.write(&reader.buffer);
+    server.write_all(&buffer).unwrap();
+    server.write_all(client.buffer()).unwrap();
 
-    let mut client_read = reader.inner;
+    let mut client_read = client.into_inner();
     let mut client_write = client_read.try_clone().unwrap();
 
     let mut server_read = server;
@@ -160,7 +107,7 @@ fn handle_client(client: TcpStream) {
             let length = client_read.read(&mut buffer).unwrap();
 
             if length > 0 {
-                server_write.write(buffer.get(0..length).unwrap());
+                server_write.write(buffer.get(0..length).unwrap()).unwrap();
             } else {
                 *c2s_connected.write().unwrap() = false;
             }
@@ -174,7 +121,7 @@ fn handle_client(client: TcpStream) {
             let length = server_read.read(&mut buffer).unwrap();
 
             if length > 0 {
-                client_write.write(buffer.get(0..length).unwrap());
+                client_write.write(buffer.get(0..length).unwrap()).unwrap();
             } else {
                 *s2c_connected.write().unwrap() = false;
             }
