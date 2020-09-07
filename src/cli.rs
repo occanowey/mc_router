9ef2c595b44a9fc2bf21dfdc6cece67b264a12bf
Griffin T <a12345678910ster@gmail.com>
2020-09-08 01:20:32 +0800

    Move commands to separete functions.

--- src/cli.rs
+++ src/cli.rs
@@ -10,40 +10,46 @@ pub fn start() {
 
         let command = parts.next().unwrap().to_lowercase();
         match command.as_str() {
-            "list" => {
-                let config = CONFIG.read().unwrap();
-
-                println!("forwards:");
-                for forward in config.forwards.iter() {
-                    println!("{} -> {}", forward.hostname, forward.target);
-                }
-            }
-
-            "forward" => {
-                let hostname = parts.next();
-                let target = parts.next();
-
-                if let (Some(hostname), Some(target)) = (hostname, target) {
-                    {
-                        let mut config = CONFIG.write().unwrap();
-                        (*config).forwards.push(Forward {
-                            hostname: hostname.to_string(),
-                            target: target.to_string(),
-                        });
-                    }
-
-                    config::save(&CONFIG.read().unwrap()).unwrap();
-                } else {
-                    println!("usage: forward <hostname> <target>");
-                }
-            }
-
-            "reload" => {
-                *CONFIG.write().unwrap() = config::load().unwrap();
-                println!("reloaded forwards");
-            }
+            "list" => execute_list(&command, &mut parts),
+            "forward" => execute_forward(&command, &mut parts),
+            "reload" => execute_reload(&command, &mut parts),
 
             _ => println!("Unknown command '{}'", command),
         }
     }
+}
+
+fn execute_list<'i, A: Iterator<Item = &'i str>>(command: &str, args: &'i mut A) {
+    let config = CONFIG.read().unwrap();
+
+    println!("forwards:");
+    for forward in config.forwards.iter() {
+        println!("{} -> {}", forward.hostname, forward.target);
+    }
+}
+
+fn execute_forward<'i, A: Iterator<Item = &'i str>>(command: &str, args: &'i mut A) {
+    let hostname = args.next();
+    let target = args.next();
+
+    if let (Some(hostname), Some(target)) = (hostname, target) {
+        {
+            let mut config = CONFIG.write().unwrap();
+            (*config).forwards.push(Forward {
+                hostname: hostname.to_string(),
+                target: target.to_string(),
+            });
+        }
+
+        config::save(&CONFIG.read().unwrap()).unwrap();
+        println!("new forward created");
+    } else {
+        println!("usage: forward <hostname> <target>");
+    }
+}
+
+
+fn execute_reload<'i, A: Iterator<Item = &'i str>>(command: &str, args: &'i mut A) {
+    *CONFIG.write().unwrap() = config::load().unwrap();
+    println!("reloaded forwards");
 }
