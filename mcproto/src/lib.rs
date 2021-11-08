6d6e34e82b7718d349f841f9ecf8c4af22dbbf54
Griffin T <a12345678910ster+git@gmail.com>
2021-11-09 03:11:31 +0800

    Split mcproto from mc_router & update hostname_validator.

--- mcproto/src/lib.rs
+++
@@ -1,9 +0,0 @@
-mod read_ext;
-mod write_ext;
-
-pub mod packet;
-
-pub use read_ext::MinecraftReadExt as ReadExt;
-pub use write_ext::MinecraftWriteExt as WriteExt;
-
-pub use packet::PacketBuilder;
